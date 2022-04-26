884770a5ad2fea8c4dde63b13eac9f89be290ef2
AymanSalah1994 <80077545+AymanSalah1994@users.noreply.github.com>
2022-04-26 17:09:29 +0200

    Sample1

---
+++ sample1.sql
@@ -0,0 +1,13 @@
+--  use the 'USE' key Word to indicate which database to use   
+USE Movies
+
+SELECT
+	FilmName FilmTitle
+	-- We Can Ignore AS KeyWord as In the Above line 
+	, FilmReleaseDate AS [Date]
+	, FilmRunTimeMinutes AS 'Timing (Minutes)'
+	-- Single Quote OR Brackets if there is a Space in Name 
+
+--It is recommended to use the AS keyword For the Readability
+--  Although you Can Omit "AS" BUT you Should use it . 
+FROM dbo.tblFilm
